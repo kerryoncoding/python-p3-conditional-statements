573f43bea1bfe8ca82ea9622581e9637d2042bf8
Kerry Guarino <kerry.guarino@gmail.com>
2023-08-24 18:31:43 -0400

    needed help on fizzbuzz

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    # here still here
-    pass
+    if ((username=="ADMIN") or (username=="admin")) and (password == "12345"):
+        return "Access granted"
+    else:
+        return "Access denied"
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if (temperature < 40):
+        return "It's brisk out there!"
+    elif (temperature > 85):
+        return "It's too dang hot out there!"
+    elif (temperature > 40 and temperature < 65):
+        return "It's a little chilly out there!"
+    else:
+        return "It's perfect out there!"
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if (not num % 3) and (not num %5):
+        return "FizzBuzz"
+    elif (not num %3 ):
+        return "Fizz"
+    elif (not num % 5):
+        return "Buzz"
+    else:
+        return num
+ 
 
 def calculator(operation, num1, num2):
     # your code here
